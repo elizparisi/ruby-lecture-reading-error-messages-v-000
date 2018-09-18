c2a70a95a14fe677372d225543af61d83b6ffa90
Elizabeth Parisi <parisielizabeth@gmail.com>
2018-09-18 21:21:37 +0000

    automatically backed up by learn

--- lib/a_type_error.rb
+++ lib/a_type_error.rb
@@ -1,3 +1,3 @@
 1 + 1
 
-1 + "is the loneliest number"
+1 + "is the loneliest https://learn.co/tracks/full-stack-web-development-v6/intro-to-ruby-development/debugging/reading-error-messages#number"
